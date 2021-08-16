8dbf8b15a35ce029b3c4575c45310167ca543104
Taher Ziyaei <ziaee.taher@gmail.com>
2021-08-16 07:03:00 +0330

    14000525 commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,28 +3,55 @@ package sample;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
-import javax.swing.text.html.ImageView;
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    private String selectedMonth;
+
+
     @FXML
     private ComboBox<String> comboMonth;
+    @FXML
+    private Label lblMonthTitle, lblMonthDomain;
 
     public void exitApplication(MouseEvent event) {
         Platform.exit();
         System.out.println("Exit from application.");
     }
 
+    public void setLblMonthDomain(Label lblMonthDomain) {
+        String textDomain = "21st ESFAND ~ 20th FARVARDIN";
+        selectedMonth = comboMonth.getSelectionModel().getSelectedItem();
+        switch (selectedMonth) {
+            case "FARVARDIN":
+                textDomain = "21st ESFAND ~ 20th FARVARDIN";
+                break;
+            case "ORDIBEHESHT":
+                textDomain = "21st FARVARDIN ~ 20th ORDIBEHESHT";
+                break;
+        }
+        lblMonthDomain.setText(textDomain);
+        this.lblMonthDomain = lblMonthDomain;
+    }
+
+    public void comboAction(ActionEvent event) {
+        selectedMonth = comboMonth.getSelectionModel().getSelectedItem();
+        lblMonthTitle.setText(String.format("%s 1400",selectedMonth));
+        setLblMonthDomain(lblMonthDomain);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         String[] persianMonths = {
                 "FARVARDIN","ORDIBEHESHT","KHORDAD",
                 "TIR","MORDAD","SHAHRIVAR",
@@ -34,5 +61,12 @@ public class Controller implements Initializable {
         ObservableList<String> months = FXCollections.observableArrayList(persianMonths);
         comboMonth.setItems(months);
         comboMonth.setValue(persianMonths[0]);
+
+        lblMonthTitle.setText(String.format("%s 1400",persianMonths[0]));
+        //        String selectedMonth = comboMonth.getSelectionModel().getSelectedItem();
+//        monthTitle.setText(String.format("%s 1400",selectedMonth));
+
     }
+
+
 }
